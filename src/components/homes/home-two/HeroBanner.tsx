a4c713b02d235eff2b330ed00da8b3d296ec2a1c
Gurneet Singh <gurneets.evo@gmail.com>
2026-01-20 10:50:20 +0530

    Update banner ratio

--- src/components/homes/home-two/HeroBanner.tsx
+++ src/components/homes/home-two/HeroBanner.tsx
@@ -13,16 +13,17 @@ const HeroBanner = () => {
    return (
       <>
          <div 
-            className="hero-banner-two z-1 position-relative" 
+            className="hero-banner-two z-1 position-relative d-flex align-items-center" 
             style={{
                backgroundImage: 'url(/banner.jpg)',
                backgroundSize: 'cover',
                backgroundPosition: 'center',
-               backgroundRepeat: 'no-repeat'
+               backgroundRepeat: 'no-repeat',
+               minHeight: '100vh'
             }}
          >
             <div className="container">
-               <div className="position-relative pt-200 md-pt-150 pb-130 xl-pb-100">
+               <div className="position-relative pt-150 pb-100">
                   <div className="row">
                      <div className="col-xxl-9 col-xl-8 col-lg-9 col-md-10">
                         {/* Content can be added here if needed */}
